8dae0ac500a8e2a996717d8d703b37f79ca1f038
leo-dev22 <lndenko18@gmail.com>
2022-06-01 06:56:45 +0400

    project ongoing

--- main.js
+++ main.js
@@ -1,36 +1,42 @@
 const result = document.getElementById("result");
 // console.log(result);
 const input = document.querySelector("input");
-console.log(input);
+//console.log(input);
 const form = document.querySelector("form");
 //console.log(form);
 let meals = [];
-async function mealsApp() {
-  await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=beef")
+async function mealsApp(search) {
+  await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=" + search)
     .then((res) => res.json())
     .then((data) => (meals = data.meals));
   console.log(meals);
 }
 function mealsDisplay() {
-  meals.length = 15;
-  result.innerHTML = meals
+  if (meals === null) {
+    result.innerHTML = "<h2>No results</h2>"
+  } else {
+
+    meals.length = 24;
+    result.innerHTML = meals
     .map(
-      (meal) => `<li class="card">
-    <h3>${meal.strMeal}</h3>
-    <p>${meal.strArea}</p>
-    <img src=${meal.strMealThumb} alt=${meal.strMeal}>
-    <ul></ul>
-    </li>
-  `
+      (meal) => `
+      <li class="card">
+      <h3>${meal.strMeal}</h3>
+      <p>${meal.strArea}</p>
+      <img src=${meal.strMealThumb} alt=${meal.strMeal}>
+      <ul></ul>
+      </li>
+      `
     )
     .join("");
+  }
 }
 input.addEventListener("input", (e) => {
   e.preventDefault();
-  mealsApp(e.target.value);
+  mealsApp(e.target.value).then(()=>mealsDisplay());
 });
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  mealsApp().then(() => mealsDisplay());
+   mealsDisplay();
 });
 
